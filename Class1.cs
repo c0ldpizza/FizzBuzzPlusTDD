1a4b38c61133bfb76c172cd3e8ccf408b223bfe1
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-20 15:22:56 -0400

    Initial commit. Functioning FizzBuzz+ after TDD

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzPlusUnitTest
+{
+    class FizzBuzzPlus
+    {
+        public static string GetFizzBuzzPlus(int x)
+        {
+
+            if (x % 15 == 0 || (x.ToString().IndexOf('3') != -1 && x.ToString().IndexOf('5') != -1))
+                return "FizzBuzz";
+            else if (x % 3 == 0 || x.ToString().IndexOf('3') != -1)
+                return "Fizz";
+            else if (x % 5 == 0 || x.ToString().IndexOf('5') != -1)
+                return "Buzz";
+            else
+                return x.ToString();
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzPlusUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string expected = "1";
+            Assert.AreEqual(expected, FizzBuzzPlus.GetFizzBuzzPlus(1));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string expected = "2";
+            Assert.AreEqual(expected, FizzBuzzPlus.GetFizzBuzzPlus(2));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string expected = "Fizz";
+            Assert.AreEqual(expected, FizzBuzzPlus.GetFizzBuzzPlus(3));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string expected = "4";
+            Assert.AreEqual(expected, FizzBuzzPlus.GetFizzBuzzPlus(4));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string expected = "Buzz";
+            Assert.AreEqual(expected, FizzBuzzPlus.GetFizzBuzzPlus(5));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string expected = "Fizz";
+            Assert.AreEqual(expected, FizzBuzzPlus.GetFizzBuzzPlus(6));
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            string expected = "Buzz";
+            Assert.AreEqual(expected, FizzBuzzPlus.GetFizzBuzzPlus(10));
+        }
+
+        [TestMethod]
+        public void TestMethod13()
+        {
+            string expected = "Fizz";
+            Assert.AreEqual(expected, FizzBuzzPlus.GetFizzBuzzPlus(13));
+        }
+
+        [TestMethod]
+        public void TestMethod15()
+        {
+            string expected = "FizzBuzz";
+            Assert.AreEqual(expected, FizzBuzzPlus.GetFizzBuzzPlus(15));
+        }
+
+        [TestMethod]
+        public void TestMethod59()
+        {
+            string expected = "Buzz";
+            Assert.AreEqual(expected, FizzBuzzPlus.GetFizzBuzzPlus(59));
+        }
+
+        [TestMethod]
+        public void TestMethod53()
+        {
+            string expected = "FizzBuzz";
+            Assert.AreEqual(expected, FizzBuzzPlus.GetFizzBuzzPlus(53));
+        }
+
+    }
+}
