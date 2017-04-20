5efebbdde738c669fd9452efe4c480c89b614275
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-20 15:31:04 -0400

    Implemented cleaner if statement solution

--- Class1.cs
+++ Class1.cs
@@ -10,15 +10,24 @@ namespace FizzBuzzPlusUnitTest
     {
         public static string GetFizzBuzzPlus(int x)
         {
+            string output = "";
 
-            if (x % 15 == 0 || (x.ToString().IndexOf('3') != -1 && x.ToString().IndexOf('5') != -1))
-                return "FizzBuzz";
-            else if (x % 3 == 0 || x.ToString().IndexOf('3') != -1)
-                return "Fizz";
-            else if (x % 5 == 0 || x.ToString().IndexOf('5') != -1)
-                return "Buzz";
-            else
-                return x.ToString();
+            if (x % 3 == 0 || x.ToString().IndexOf('3') != -1)
+                output += "Fizz";
+            if (x % 5 == 0 || x.ToString().IndexOf('5') != -1)
+                output += "Buzz";
+            if (output == "")
+                output = x.ToString();
+            return output;
+
+            //if (x % 15 == 0 || (x.ToString().IndexOf('3') != -1 && x.ToString().IndexOf('5') != -1))
+            //    return "FizzBuzz";
+            //else if (x % 3 == 0 || x.ToString().IndexOf('3') != -1)
+            //    return "Fizz";
+            //else if (x % 5 == 0 || x.ToString().IndexOf('5') != -1)
+            //    return "Buzz";
+            //else
+            //    return x.ToString();
         }
     }
 }
